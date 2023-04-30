997f6d9ac67d505403d6f68f1a2c93229cde3b9d
Polina Kharchenko <polina.kharchenko@nure.ua>
2023-04-30 19:54:27 +0300

    fix naming

---
+++ RentEase.DAL/Migrations/20230430165357_FixNaming.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentEase.DAL.Migrations
+{
+    public partial class FixNaming : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeliveryAdress",
+                table: "Orders",
+                newName: "DeliveryAddress");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeliveryAddress",
+                table: "Orders",
+                newName: "DeliveryAdress");
+        }
+    }
+}
