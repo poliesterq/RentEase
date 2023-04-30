80e9ad4a642679dd3a845e4db315c0906b69243a
Polina Kharchenko <polina.kharchenko@nure.ua>
2023-04-30 16:34:26 +0300

    Add logging, exception middleware

---
+++ RentEase.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+
+namespace RentEase.API.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+        IHostEnvironment env)
+    {
+        _env = env;
+        _logger = logger;
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            if (_env.IsDevelopment())
+            {
+                context.Response.ContentType = System.Net.Mime.MediaTypeNames.Text.Plain;
+                await context.Response.WriteAsync(ex.ToString());
+            }
+            else
+            {
+                await context.Response.WriteAsJsonAsync("Oops, something went wrong.");
+            }
+        }
+    }
+}
--- RentEase.API/Program.cs
+++ RentEase.API/Program.cs
@@ -1,13 +1,46 @@
+using System.Text.Json.Serialization;
+using RentEase.API.Middlewares;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Add logging
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 
+builder.Logging.ClearProviders();
+
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(config)
+    .WriteTo.Console()
+    .WriteTo.File($"../Logs/.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+//Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +50,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseDefaultFiles();
+
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
