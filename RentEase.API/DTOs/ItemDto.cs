90d12964b825f9a193751d5e229d8bd4c19defdb
Polina Kharchenko <polina.kharchenko@nure.ua>
2023-05-01 01:08:07 +0300

    implement item

---
+++ RentEase.API/DTOs/ItemDto.cs
@@ -0,0 +1,22 @@
+﻿using RentEase.Domain.Enums;
+
+namespace RentEase.API.DTOs;
+
+public class ItemDto
+{
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    
+    public Category Category { get; set; }
+    
+    public int PriceUS { get; set; }
+    
+    public string Address { get; set; }
+    
+    public string ImageUrl { get; set; }
+    
+    public string LandlordId { get; set; } 
+    
+    public bool IsAvailable { get; set; }
+}
---
+++ RentEase.Domain/Models/Request/ItemSearchParameter.cs
@@ -0,0 +1,24 @@
+﻿using RentEase.Domain.Enums;
+
+namespace RentEase.Domain.Models.Request;
+
+public class ItemSearchParameters
+{
+    public string? SearchParameter { get; set; }
+    
+    public List<Category>? Categories { get; set; }
+    
+    public string? Address { get; set; }
+    
+    public int? PriceMinUS { get; set; }
+    
+    public int? PriceMaxUS { get; set; }
+    
+    //public int? PriceMinUA { get; set; }
+    
+    //public int? PriceMaxUA { get; set; }
+    
+    public string? LandlordId { get; set; }
+    
+    public bool? IsAvailable { get; set; }
+}
